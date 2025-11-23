5dbbea3f969c4796beef5fe4fa70941cdff28a94
jzhs <jonstenerson@comcast.net>
2025-11-23 04:18:38 -0700

    import register module from sap-1-3

---
+++ src/register.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+`default_nettype none 
+
+module register
+#(parameter size = 8)
+(
+  input wire sysclk,
+  input wire reset,
+  input wire clken,
+  input wire load,
+  input  wire [size-1:0] data_in,
+  output reg [size-1:0] value
+);
+
+always @(posedge sysclk or posedge reset)
+begin
+   if (reset) begin
+      value <= 0;
+   end else if (clken) begin
+      if (load)
+	value <= data_in;
+      else
+	value <= value;
+   end
+end
+
+endmodule
